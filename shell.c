a2a0e85303700bd1515c49e9ba8f88024e9d13e8
4zv4l <simon.blacks.be@gmail.com>
2021-06-11 18:05:51 +0200

    more comment + put the lib in a folder

--- shell.c
+++ shell.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include "cmd.c"
+#include "pkg/cmd.c"
 
 int main(void){
     int err;
-    while(err != -1){
+    while(err != -1){ // exit will return -1
         err = get_cmd();
     }
 }
